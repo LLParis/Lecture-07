b9c6af70f59a247d222c25113a1a332c819c335c
Your Name <you@example.com>
2024-04-25 13:30:32 -0700

    Work In Progress

--- src/Lecture07.java
+++ src/Lecture07.java
@@ -6,12 +6,20 @@ class Lecture07 {
         // Note: you may need to put some of your answers inside comments
 
         // Put your answer for #8 here:
+        // A Call stack is a data structure that operates on the Last in First out principle.
+        // It relates to recursion by unwinding after all the calls have been pushed sequentially on to the stack.
 
         // Put your answer for #9 here:
+        // This is a matter of printing before or after the recursive call. After each line is read,
+        // it is immediately printed. There is no reversal, the recursion just reads the line and prints.
 
         // Put your answer for #10 here:
+        // The method is recursively stacking the lines, after checking if there is a line, until there are no more.
+        // Then, because the print is after the recursion, the stack unwinds printing from last to first.
 
         // Put your answer for #11 here:
+        // There is no base case, it doesn't handle the edge case of y = 0
+        // It can be fixed by adding an if statement for y = 0
 
         // Put your answer for #17 here:
 
